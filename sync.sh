72ed541441d7689e38bd3c5b1c6f61e3369cc0de
arxanas <arxanas@gmail.com>
2014-07-11 00:48:35 -0400

    Set file modified times to a date in the far past.

--- sync.sh
+++ sync.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+find . -not -wholename "*.git*" -not -name "sync.sh" -print0 | xargs -0 touch -t 0101011234.56
 rsync -Cavz --exclude="sync.sh" --exclude=".git" --delete . me-waleedkhan:home/resume
